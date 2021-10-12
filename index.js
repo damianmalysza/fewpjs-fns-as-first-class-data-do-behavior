31df8cfc61312baf827a9c8b4b6e172f1fcb631a
Damian Malysza <damian.malysza@gmail.com>
2021-10-12 15:47:05 -0400

    commit tree c1fe9e7cf40535d8504a58d8858c2a7391e3523d

--- index.js
+++ index.js
@@ -8,6 +8,22 @@ function handleClick(e) {
 }
 
 /* End Given Code, don't edit above here...*/
+function greet(time){
+  const hour = parseInt(time.split(':')[0])
+  if (hour < 12) {
+    return 'Good Morning'
+  } else if (hour > 18){
+    return 'Good Evening'
+  } else {
+    return 'Good Afternoon'
+  }
+}
 
 /* Write your implementation of greet() */
+
+function displayMessage(message){
+  const greetingNode = document.getElementById("greeting")
+  greetingNode.innerHTML = message
+}
+
 /* Write your implementation of displayMessage() */
